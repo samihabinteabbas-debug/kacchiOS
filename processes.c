fb5caf8fce59f11fb7a2e0827ce97fa1c1c6fdce
samihabinteabbas <samihabinteabbas@gmail.com>
2026-01-04 01:48:14 +0600

    Update processes.c

--- processes.c
+++ processes.c
@@ -2,14 +2,14 @@
 
 void procA(void) {
     while (1) {
-        serial_puts("A\n");
+        serial_puts("A running...\n");
         for (volatile int i = 0; i < 1000000; i++);
     }
 }
 
 void procB(void) {
     while (1) {
-        serial_puts("B\n");
+        serial_puts("B running...\n");
         for (volatile int i = 0; i < 1000000; i++);
     }
 }
