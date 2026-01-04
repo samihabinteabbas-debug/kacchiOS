cfee0dda626320b58550371f4e2fd3b741c61a56
samihabinteabbas <samihabinteabbas@gmail.com>
2026-01-04 14:24:00 +0600

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -16,9 +16,9 @@ void kmain(void) {
     serial_init();
     memory_init();
     process_init();
-    process_create(procA, 3);
-    process_create(procB, 5);
-    schedule();
+    //process_create(procA, 3);
+    //process_create(procB, 5);
+    //schedule();
     /* Print welcome message */
     serial_puts("\n");
     serial_puts("========================================\n");
