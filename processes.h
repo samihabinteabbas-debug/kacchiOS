507fa486b97038001785f6122ca4041a2a2480a2
samihabinteabbas <samihabinteabbas@gmail.com>
2026-01-05 15:46:52 +0600

    Update processes.h

--- processes.h
+++ processes.h
@@ -1,7 +1,10 @@
 #ifndef PROCESSES_H
 #define PROCESSES_H
 
-void procA(void);
-void procB(void);
+/* Demo processes */
+void procA(void);  /* Producer - sends IPC messages */
+void procB(void);  /* Consumer - receives IPC messages */
+void procC(void);  /* Memory tester */
+void procD(void);  /* Status reporter */
 
 #endif
