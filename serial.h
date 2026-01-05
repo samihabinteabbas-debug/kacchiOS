634c3a0ef62af88300d8199e27dfaededd76b933
samihabinteabbas <samihabinteabbas@gmail.com>
2026-01-05 15:17:32 +0600

    Update serial.h

--- serial.h
+++ serial.h
@@ -9,10 +9,5 @@ void serial_putc(char c);
 void serial_puts(const char* str);
 char serial_getc(void);
 void serial_putu(unsigned int x);
-void serial_puthex(uint32_t val) {
-    char hex[] = "0123456789ABCDEF";
-    for (int i = 28; i >= 0; i -= 4) {
-        serial_putc(hex[(val >> i) & 0xF]);
-    }
-}
+
 #endif
