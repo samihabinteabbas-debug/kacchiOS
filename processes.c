d323ef0641a39ee495821da267eab924ba1a12f2
samihabinteabbas <samihabinteabbas@gmail.com>
2026-01-05 14:36:37 +0600

    Update processes.c

--- processes.c
+++ processes.c
@@ -1,18 +1,21 @@
-#include "serial.h"
-#include "processes.h"
-#include "scheduler.h"
 void procA(void) {
-    serial_puts("A running...\n");
+    while (1) {
+        serial_puts("A running...\n");
 
-    for (volatile int i = 0; i < 1000000; i++);
+        for (volatile int i = 0; i < 1000000; i++);
 
-    serial_puts("A done...\n");
-    schedule();
+        serial_puts("A done...\n");
+        schedule();
+    }
 }
 
 void procB(void) {
-    serial_puts("B running...\n");    
-    for (volatile int i = 0; i < 1000000; i++);
-    serial_puts("B done...\n");
-    schedule();
+    while (1) {
+        serial_puts("B running...\n");
+
+        for (volatile int i = 0; i < 1000000; i++);
+
+        serial_puts("B done...\n");
+        schedule();
+    }
 }
