597ba9162c6ee908fa0833def13299a67156316a
samihabinteabbas <samihabinteabbas@gmail.com>
2026-01-05 15:25:54 +0600

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -7,7 +7,12 @@
 #include "scheduler.h"
 #include "memory.h"
 #include "processes.h"
-
+void serial_puthex(uint32_t val) {
+    char hex[] = "0123456789ABCDEF";
+    for (int i = 28; i >= 0; i -= 4) {
+        serial_putc(hex[(val >> i) & 0xF]);
+    }
+}
 /* -------------------------------------------------- */
 /* Null / Idle process                                */
 /* -------------------------------------------------- */
@@ -39,6 +44,10 @@ void kmain(void) {
      * Create processes
      * IMPORTANT: null process must be created FIRST
      */
+    serial_puts("Function addresses:\n");
+serial_puts("  procA = 0x");
+serial_puthex((uint32_t)procA);
+serial_puts("\n");
     process_create(null_process, 0);   /* PID 0 : idle */
     process_create(procA, 3);           /* user process */
     // process_create(procB, 5);
